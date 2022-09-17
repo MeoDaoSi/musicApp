1aefb6938cd1492c9f8ffed9b398497ba35d0b08
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-09-17 17:59:51 +0700

    create workspace

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const cors = require('cors');
+
+app.get('/', (req, res) => {
+    return res.json("Hello there......")
+})
+
+app.listen(4000, () => {
+    console.log('listening to port 4000');
+})
