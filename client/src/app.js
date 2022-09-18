7fb48d2a10b48298d313993db565546345a4cc39
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2022-09-18 11:10:26 +0700

    login part1

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home, Login } from './components'
 
 const App = () => {
     return (
-        <div className='w-screen h-screen bg-blue-400 flex justify-center items-center'>App</div>
+        <div className='w-screen h-screen bg-primary flex justify-center items-center'>
+            <Routes>
+                <Route path='/login' element={<Login />}/>
+                <Route path='/*' element={<Home />}/>
+            </Routes>
+        </div>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app'
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>
 , document.getElementById('root'))
